12199159540400d4841fde782dde490ddfc2d0f0
Dmitriy Rotatyy <flamine@list.ru>
2016-01-07 15:06:11 +0300

    create new models: questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106132258) do
+ActiveRecord::Schema.define(version: 20160107120423) do
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "test_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sections", force: :cascade do |t|
     t.string   "name"
