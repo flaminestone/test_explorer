32467b70355eeb017507ae120b5c5d3a80da8b3a
Dmitriy Rotatyy <flamine@list.ru>
2016-01-08 17:05:14 +0300

    return to edit test page after add new variant

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -31,7 +31,7 @@ class VariantsController < ApplicationController
     respond_to do |format|
       if @variant.save
         get_question.variants << @variant
-        format.html { redirect_to  question_variant_path(get_question, @variant), notice: 'Variant was successfully created.' }
+        format.html { redirect_to edit_section_test_path(get_section, get_test), notice: 'Variant was successfully created.' }
         format.json { render :show, status: :created, location: @variant }
       else
         format.html { render :new }
@@ -45,7 +45,7 @@ class VariantsController < ApplicationController
   def update
     respond_to do |format|
       if @variant.update(variant_params)
-        format.html { redirect_to question_variant_path(get_question, @variant), notice: 'Variant was successfully updated.' }
+        format.html { redirect_to edit_section_test_path(get_section, get_test), notice: 'Variant was successfully updated.' }
         format.json { render :show, status: :ok, location: @variant }
       else
         format.html { render :edit }
@@ -65,17 +65,25 @@ class VariantsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_variant
-      @variant = Variant.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_variant
+    @variant = Variant.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def variant_params
-      params.require(:variant).permit(:name, :answer, :avatar)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def variant_params
+    params.require(:variant).permit(:name, :answer, :avatar)
+  end
 
-    def get_question
-      Question.find(params.require(:question_id))
-    end
+  def get_question
+    Question.find(params.require(:question_id))
+  end
+
+  def get_section
+    Section.find(get_test.section_id)
+  end
+
+  def get_test
+    Test.find(get_question.test_id)
+  end
 end
