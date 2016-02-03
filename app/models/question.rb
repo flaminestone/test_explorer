2aca9b04e73a640cda024829ab21037166f3be60
Dmitriy Rotatyy <flamine@list.ru>
2016-02-04 01:04:49 +0300

    add validation and errors message

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
+  validates :name, presence: {message: 'Название вопроса не может быть пустым'}
   belongs_to :test
   has_many :variants
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,5 @@
 class Test < ActiveRecord::Base
+  validates :name, presence: {message: 'Название теста не может быть пустым'}
   belongs_to :section
   has_many :questions
 end
