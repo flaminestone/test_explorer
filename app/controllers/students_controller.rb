45ba81a801cafb8271d13a5aa9472c613c08b127
flaminestone <flamine@list.ru>
2017-06-11 18:39:02 +0300

    Delete auth fot theater if user want to run test

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:show, :index, :new]
 
   def new
     @student = Student.new
